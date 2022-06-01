da56ef2cfb2ac24978c6319e7048c9ea4cb5be11
Patel smit <patelsmit15150@gmail.com>
2022-06-02 00:16:32 +0530

    add

--- carzone/page/urls.py
+++ carzone/page/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('services/', views.services, name='services'),
+    path('contact/', views.contact, name='contact'),
 ]
 
--- carzone/page/views.py
+++ carzone/page/views.py
@@ -2,5 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
+def home(request):
     return render(request, 'page/home.html')
+
+def about(request):
+    return render(request, 'page/about.html')
+
+def services(request):
+    return render(request, 'page/services.html')
+
+def contact(request):
+    return render(request, 'page/contact.html')
