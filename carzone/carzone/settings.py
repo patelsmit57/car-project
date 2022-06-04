dd82341b0576618b5ef79cc9b6c3f92dbe5dd2c9
Patel smit <patelsmit15150@gmail.com>
2022-06-04 12:07:36 +0530

    add car data

--- carzone/carzone/settings.py
+++ carzone/carzone/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ckeditor',
     'colorfield',
+    'django.contrib.humanize',
     'page',
     'cars',
 ]
--- carzone/page/views.py
+++ carzone/page/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from .models import TeamModel
+from cars.models import CarModel
 
 # Create your views here.
 
 def home(request):
     team = TeamModel.objects.all()
+    feature_cars = CarModel.objects.order_by('-created_date').filter(is_featured = True)
+    all_cars = CarModel.objects.order_by('-created_date')
     data = {
-        'teams' : team
+        'teams' : team,
+        'feature_cars' : feature_cars,
+        'all_cars' : all_cars,
     }
     return render(request, 'page/home.html', data)
 
