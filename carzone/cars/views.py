54ae582462c79d5f9c8faf4159a3c2e0385b2c59
Patel smit <patelsmit15150@gmail.com>
2022-06-05 15:41:39 +0530

    Single Car page ead

--- carzone/cars/views.py
+++ carzone/cars/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import CarModel
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 # Create your views here.
 
 def cars(request):
-    return render(request, 'cars/cars.html')
+    cars = CarModel.objects.order_by('-created_date')
+    # paginator = Paginator(cars,4,orphans=2)
+    paginator = Paginator(cars,4)
+    page = request.GET.get('page')
+    page_cars = paginator.get_page(page)
+    data = {
+        'cars' : page_cars
+    }
+    return render(request, 'cars/cars.html', data)
+
+def car_detail(request, id):
+    single_car = get_object_or_404(CarModel, pk=id)
+    data = {
+        'single_car' : single_car
+    }
+    return render(request, 'cars/car_detail.html', data)
